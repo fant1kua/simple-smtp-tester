e68946d493b2b8f121187f1b04cb51bce31e360d
fant1kua <fant1kua@users.noreply.github.com>
2019-06-28 01:20:55 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const SMTPServer = require('smtp-server').SMTPServer;
+const simpleParser = require('mailparser').simpleParser;
+const previewEmail = require('preview-email');
+const pino = require('pino');
+const yargs = require('yargs');
+
+const pretty = pino.pretty();
+pretty.pipe(process.stdout)
+const logger = pino({
+	name: 'smtp-tester',
+	safe: true
+}, pretty);
+
+const server = new SMTPServer({
+	disabledCommands: [ 'AUTH', 'STARTTLS' ],
+
+	onAuth(auth, session, callback) {
+		callback(null);
+	},
+
+	onMailFrom(address, session, callback) {
+		callback(null);
+	},
+
+	onData(stream, session, callback) {
+		const chunks = [];
+
+		stream.on('data', buffer => {
+			chunks.push(buffer);
+		});
+
+		stream.on('error', err => {
+			logger.error('Error while recieve email', err);
+		})
+
+		stream.on('end', () => {
+			const data = Buffer.concat(chunks).toString().replace(/\r\n$/, '');
+			logger.info('Email recieved successfully');
+
+			simpleParser(data)
+				.then(mail => {
+					logger.info(`Email ${mail.subject} from ${mail.from.text} to ${ mail.to.text} parsed`);
+					return previewEmail({
+						from: mail.from.text,
+						to: mail.to.text,
+						subject: mail.subject,
+						html: mail.html,
+						text: mail.text,
+					})
+				})
+				.then(() => {
+					logger.info('Email recieve finished');
+					callback(null);
+				})
+				.catch(err => {
+					logger.error('Error while parsing email', err);
+					callback(err);
+				});
+		});
+	}
+});
+
+server.on('error', err => {
+    logger.error('SMTP server error', err);
+});
+
+const argv = yargs(['--port'].concat(JSON.parse(process.env.npm_config_argv).remain)).argv;
+const port = Number.isInteger(argv.port) ? argv.port : process.env.npm_package_config_port;
+
+server.listen(port, err => {
+	if (err) {
+		logger.error('Error while starting SMTP server', err);
+	} else {
+		logger.info(`SMTP server started successfully on port ${port}`);
+	}
+});
